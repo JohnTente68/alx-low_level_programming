79e1eabba5e039cb9d63f0464766f74a1fc6048a
JohnTente68 <tentemakume@gmail.com>
2023-09-12 04:02:14 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+     // Get the character to be written
+     char ch = 'G';
+
+    // Write the Character to stdout
+    putchar(ch);
+
+    return (0);
+}    
